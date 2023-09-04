82079ca5fb895192007235c6e318a450ff4cad30
burkuts <83317998+burkuts@users.noreply.github.com>
2023-09-05 01:14:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-function guncelleSaat() {
-    const saatDiv = document.getElementById("saat");
-    const tarih = new Date();
-    const saat = tarih.getHours();
-    const dakika = tarih.getMinutes();
-    const saniye = tarih.getSeconds();
+let intervalId;
+let isRunning = false;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
-    const saatMetni = `${saat.toString().padStart(2, "0")}:${dakika.toString().padStart(2, "0")}:${saniye.toString().padStart(2, "0")}`;
-    saatDiv.textContent = `Türkiye Saati: ${saatMetni}`;
+const zamanEkrani = document.getElementById("zaman");
+const basDurdurButonu = document.getElementById("basDurdur");
+const sifirlaButonu = document.getElementById("sifirla");
+
+function basDurdurKronometre() {
+    if (isRunning) {
+        clearInterval(intervalId);
+        basDurdurButonu.textContent = "Başlat";
+    } else {
+        intervalId = setInterval(artirZamani, 1000);
+        basDurdurButonu.textContent = "Durdur";
+    }
+    isRunning = !isRunning;
 }
 
-setInterval(guncelleSaat, 1000); // Saati her saniye güncelle
-guncelleSaat(); // Sayfa yüklendiğinde saat bilgisini hemen güncelle
+function sifirlaKronometre() {
+    clearInterval(intervalId);
+    isRunning = false;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    zamanEkrani.textContent = "00:00:00";
+    basDurdurButonu.textContent = "Başlat";
+}
+
+function artirZamani() {
+    seconds++;
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    zamanEkrani.textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+basDurdurButonu.addEventListener("click", basDurdurKronometre);
+sifirlaButonu.addEventListener("click", sifirlaKronometre);
